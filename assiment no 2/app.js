2db3ed412fc59cc47efaedab939e64db203b9a6f
Umarkhan65 <122873840+Umarkhan65@users.noreply.github.com>
2023-01-28 02:13:41 -0800

    Add files via upload

---
+++ assiment no 2/app.js
@@ -0,0 +1,69 @@
+                                //*******CHAPTER NO-2
+// ANSWER NO-1
+// var name = ("umar khan");
+// console.log(name);
+
+// ANSWER NO-2
+// var xyz = "user name"
+// console.log (xyz)
+
+// ANSWER NO-3
+// alert ("hello world")
+
+// ANSWER NO-4
+// alert ("sir ghose")
+
+// ANSWER NO-5
+// alert ("15 year old")
+// alert ("certified mobile application devlopment")
+
+// ANSWER NO-6
+// var lol ="pizza \n pizz \n piz \n pi \n p"
+// console.log (lol)
+
+// ANSWER NO-7
+// alert ("umaraamir959@gmil.com")
+
+// ANSWER NO-8
+// alert ("I am trying to learn the book a smarter way to learn javascript")
+
+// ANSWER NO-9
+// document.write("yeh! i can write HTML content throught javascript")
+
+// ANSWEWR NO=10
+// alert ("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+
+                        // CHAPER************************ NO-3
+
+
+// ANSWER NO-1
+// var num = 'I am 15 year old'
+// alert (num)
+
+// ANSWER NO-2
+// var num ="you have visit this site 14 times"
+// alert (num)
+
+// ANSWER NO-3
+// document.write ("MY birth year is 2006 <br /> deta type is my ddedeclard veriable is number,")
+
+// ANSWER NO-4
+// document.write (" <p> <b> jhon deo <b/> order <b> 5-t short <b/> (s) on xyz clothing store <p/>")
+
+                        //CHAPTER*************************** NO-4
+
+// ANSWER NO-1
+// var $myFirstVeriable = ("Hello world")
+// console.log($myFirstVeriable)
+
+// ANSWER NO-2
+// var userResponse_userResponseTime_userResponseTimeLimit_response = ("lligel name")
+// console.log(userResponse_userResponseTime_userResponseTimeLimit_response)
+    // var veriable_alert_console_log_document = ("illigel name")
+    // console.log( veriable_alert_console_log_document)
+
+// ANSWER NO-3
+// document.write("<h1> A heading stating “Rules for naming JS variables")
+
+
